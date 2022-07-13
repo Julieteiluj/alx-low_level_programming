de3bf3c6e33dc11446508551e6574347ad2b0397
Julieteiluj <ndukwejuliet41@gmail.com>
2022-07-13 07:28:30 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -23,9 +23,9 @@ int main(void)
 		pass[x] = rand() % 78;
 		sum += (pass[x] + '0');
 		putchar(pass[x] + '0');
-		if ((2722 - sum) - '0' < 78)
+		if ((2772 - sum) - '0' < 78)
 		{
-			y = 2722 - sum - '0';
+			y = 2772 - sum - '0';
 			sum += y;
 			putchar(y + '0');
 			break;
