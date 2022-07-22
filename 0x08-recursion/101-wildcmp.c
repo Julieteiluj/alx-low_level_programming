aeafc648398ad17b0515d6b75b45f6e12439f55a
Julieteiluj <ndukwejuliet41@gmail.com>
2022-07-21 21:12:19 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,22 +10,22 @@
  */
 int str_checker(char *s1, char *s2, int i, int j)
 {
-if (s1[i] == '\0' && s2[j] == '\0')
-return (1);
-if (s1[i] == s2[j])
-	return (str_checker(s1, s2, i + 1, j + 1));
-if (s1[i] == '\0' && s2[j] == '*')
-	return (str_checker(s1, s2, i, j + 1));
-if (s2[j] == '*')
-	return (str_checker9s1, s2, i + 1, j || str_checker(s1, s2, i, j + 1));
-return (0);
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
 }
 /**
  * wildcmp - check if strings could be considered identical
  * @s1: base address for string.
  * @s2: base address for string.
  *
- * Return: 1 if are considered identical
+ * Return: 1 if are considered identical.
  */
 int wildcmp(char *s1, char *s2)
 {
